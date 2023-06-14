873b722f713e3327edaf343515180f598ca4edaf
Thinh Nguyen <thinhtrung.nguyen1701@gmail.com>
2023-06-13 17:15:10 -0700

    a reponsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="gold">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="pink">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="tomato">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
